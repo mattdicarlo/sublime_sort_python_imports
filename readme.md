c7a5a81c692bc6b04ae512ca264000a5be38a212
Matt DiCarlo <dicarlo.matt@gmail.com>
2022-03-09 20:22:02 -0600

    Load isort config from file

--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
 # Sort Python Imports plugin for Sublime Text 4
 
-Simple plugin to run `isort` on the current Python file. Uses my preferred `isort` configuration for now.
+Simple plugin to run `isort` on the current Python file.
 
 # Usage
 
 Open the Command Pallete and run "Sort Python Imports".
 
+If the current view has a file path, it will be passed to isort to [find a configuration file](https://pycqa.github.io/isort/docs/configuration/config_files.html), if one exists.
+
 # TODO
 
-- Configuration via ST settings
+- Configuration via ST settings if isort config file is not found.
 - Actually require ST4 since it won't run on earlier versions due to Python 3.8 requirement.
-- Handle isort configuration from user files (see [CONFIG_SOURCES in settings.py](./_vendor/isort/settings.py)).
--- sublime_sort_python_imports.py
+++ sublime_sort_python_imports.py
@@ -1,33 +1,52 @@
 import os
+import sys
+from pathlib import Path
+
 import sublime
 import sublime_plugin
-import sys
 
 
-isort_config: dict = {}
+isort_config_overrides: dict = {}
 
 
 class SortPythonImportsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # Only run on python files.
         syntax = self.view.syntax()
-        if syntax is not None and syntax.name == 'Python':
-            setup_vendored_lib_path()
-            import isort
+        if syntax is None or syntax.name != 'Python':
+            sublime.error_message('Not a Python file.')
+            return
 
-            region = sublime.Region(0, self.view.size())
-            content = self.view.substr(region)
+        setup_vendored_lib_path()
+        import isort
+        clear_isort_caches()
 
-            output = isort.code(content, **isort_config)
+        region = sublime.Region(0, self.view.size())
+        content = self.view.substr(region)
 
+        file_path = self.view.file_name()
+        if file_path and len(file_path) > 0:
+            # If the view has a file path, get the directory containing it so
+            # that isort's logic for finding configuration will operate from there.
+            file_path = Path(file_path).resolve()
+            if file_path.is_file():
+                file_path = file_path.parent
+        else:
+            file_path = None
+
+        print(f'file_path={file_path}')
+        output = isort.code(
+            content,
+            file_path=file_path,
+            **isort_config_overrides
+        )
+
+        if content != output:
             self.view.replace(
                 edit,
                 region,
                 output
             )
-
-        else:
-            sublime.error_message('Not a Python file.')
 
 
 def setup_vendored_lib_path():
@@ -36,15 +55,27 @@ def setup_vendored_lib_path():
     sys.path.append(vendor_dir)
 
 
+def clear_isort_caches():
+    """ isort uses functools.lru_cache on the functions that load settings from file.
+        We want to re-read the file any time the plugin is run in case it has been changed,
+        but I haven't found a way to force that other than importing these functions
+        and clearing the cache.
+    """
+
+    from isort.settings import _find_config, _get_config_data, find_all_configs
+    for fn in (_find_config, find_all_configs, _get_config_data):
+        fn.cache_clear()
+
+
 def plugin_loaded():
     # plugin_settings = sublime.load_settings(SETTINGS_FILE)
 
-    global isort_config
-    isort_config = {
-        'no_sections': True,
-        'lines_after_imports': 2,
-        'lines_between_types': 0,
-        'include_trailing_comma': True,
-        'force_alphabetical_sort': True,
-        'multi_line_output': 5,
+    global isort_config_overrides
+    isort_config_overrides = {
+        # 'include_trailing_comma': True,
+        # 'lines_after_imports': 2,
+        # 'lines_between_sections': 1,
+        # 'lines_between_types': 0,
+        # 'multi_line_output': 3,
+        # 'no_sections': False,
     }
